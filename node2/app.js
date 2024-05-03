006188054aa5d34c2666b2518c1da67052d43cfa
Derek O Brien <obrien.derek1@gmail.com>
2024-05-03 20:24:59 +0100

    update java and node projs

--- node2/app.js
+++ node2/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my server!!');
+  res.send('Welcome to my server2!!');
 });
 
 app.get('/me', (req, res) =>{
